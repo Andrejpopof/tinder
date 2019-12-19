4f730e1d76adabe2382ad5155ea437b624007de1
Andrejpopof <andrejpopov.1430@gmail.com>
2019-12-19 13:23:03 +0100

    Added pictures for comments

--- app/src/main/java/com/example/proektandroiddavidpope/DetailsActivity.java
+++ app/src/main/java/com/example/proektandroiddavidpope/DetailsActivity.java
@@ -20,9 +20,17 @@ public class DetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_details);
 
         ArrayList<Element> komentari =new ArrayList<>();
-        komentari.add(new Element(R.drawable.ic_android_black_24dp,"comment1"));
-        komentari.add(new Element(R.drawable.ic_block_black_24dp,"comment2"));
-
+        komentari.add(new Element(R.drawable.kd,"comment1"));
+        komentari.add(new Element(R.drawable.kuzma,"comment2"));
+        komentari.add(new Element(R.drawable.charly,"comment2"));
+        komentari.add(new Element(R.drawable.lj,"comment2"));
+        komentari.add(new Element(R.drawable.daisy,"comment2"));
+        komentari.add(new Element(R.drawable.russ,"comment2"));
+        komentari.add(new Element(R.drawable.klay,"comment2"));
+        komentari.add(new Element(R.drawable.dileta,"comment2"));
+        komentari.add(new Element(R.drawable.charly,"comment2"));
+        komentari.add(new Element(R.drawable.russ,"comment2"));
+        komentari.add(new Element(R.drawable.lj,"comment2"));
 
 
         mRecyclerView=findViewById(R.id.recyclerView);
